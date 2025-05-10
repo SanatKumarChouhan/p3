664ac24521683a1f0ab17e377b395dc4ce2e4444
Sanat Kumar Chouhan <159426924+SanatKumarChouhan@users.noreply.github.com>
2025-05-11 01:21:02 +0530

    Update JasperCtl.java

--- project_3/src/main/java/in/co/rays/project_3/controller/JasperCtl.java
+++ project_3/src/main/java/in/co/rays/project_3/controller/JasperCtl.java
@@ -45,7 +45,7 @@ public class JasperCtl extends BaseCtl {
 
 			/* Compilation of jrxml file */
 			JasperReport jasperReport = JasperCompileManager
-					.compileReport("C:\\Users\\Asus\\JaspersoftWorkspace\\MyReports\\B.jrxml");
+					.compileReport("C:\\Users\\Asus\\JaspersoftWorkspace\\MyReports\\BB.jrxml");
 
 			HttpSession session = request.getSession(true);
 			UserDTO dto = (UserDTO) session.getAttribute("user");
